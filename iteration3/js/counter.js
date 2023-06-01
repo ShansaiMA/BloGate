c648348084674692258bffd53a04996ffe561843
Shansai Mahendran <shansai.mahendran@hs-ruhrwest.de>
2023-06-01 20:00:31 +0200

    Code Best Practice
    Die Zeilen kommentiert

--- iteration3/js/counter.js
+++ iteration3/js/counter.js
@@ -1,9 +1,16 @@
-//ändern
+//ändern     
+
+//Variablen deklarieren und auf die DOM-Elemente mit der ID "kommentar" und "zaheler" zugreifen
 var kommentarTextfeld = document.getElementById("kommentar");
- var zaehler = document.getElementById("zaehler");
- 
- kommentarTextfeld.addEventListener("input", function() {
-   var eingegebenerText = kommentarTextfeld.value;
-   var zeichenAnzahl = eingegebenerText.length;
-   zaehler.innerText = zeichenAnzahl;
- });
+var zaehler = document.getElementById("zaehler");
+
+// EventListener hinzugefügt, sodass auf Eingabe im Textfeld reagiert wird
+kommentarTextfeld.addEventListener("input", function () {
+
+  //eingegeben Text aus dem Textfeld abrufen
+  var eingegebenerText = kommentarTextfeld.value;
+  //Anzahl der Zeichen ermitteln
+  var zeichenAnzahl = eingegebenerText.length;
+  // Anzahl der Zeichen anzeigen
+  zaehler.innerText = zeichenAnzahl;
+});
