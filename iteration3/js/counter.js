8c75b91cf081e4bd3751721b631e4a157a94f5db
Shansai Mahendran <shansai.mahendran@hs-ruhrwest.de>
2023-06-01 19:26:31 +0200

    Zeichenzähler fertig

--- iteration3/js/counter.js
+++ iteration3/js/counter.js
@@ -1,2 +1,9 @@
-alert('HALLO!');
-
+//ändern
+var kommentarTextfeld = document.getElementById("kommentar");
+ var zaehler = document.getElementById("zaehler");
+ 
+ kommentarTextfeld.addEventListener("input", function() {
+   var eingegebenerText = kommentarTextfeld.value;
+   var zeichenAnzahl = eingegebenerText.length;
+   zaehler.innerText = zeichenAnzahl;
+ });
