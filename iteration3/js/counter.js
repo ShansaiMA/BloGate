959d4611a66f6549e888b2445b947bb20b35587e
Shansai Mahendran <shansai.mahendran@hs-ruhrwest.de>
2023-06-04 23:08:12 +0200

    Format Document benutzt, Javascript eingebunden
    Favoriten Tabelle auskommentiert, damit es erst eingefügt wird, wenn der Nutzer auf Favoriten hinzufügen drückt,

--- iteration3/js/counter.js
+++ iteration3/js/counter.js
@@ -1,16 +1,15 @@
-//ändern     
+//ändern     //VAR geändert zu Const, da COde best practice
 
 //Variablen deklarieren und auf die DOM-Elemente mit der ID "kommentar" und "zaheler" zugreifen
-var kommentarTextfeld = document.getElementById("kommentar");
-var zaehler = document.getElementById("zaehler");
+const kommentarTextfeld = document.getElementById("kommentar");
+const zaehler = document.getElementById("zaehler");
 
 // EventListener hinzugefügt, sodass auf Eingabe im Textfeld reagiert wird
 kommentarTextfeld.addEventListener("input", function () {
-
   //eingegeben Text aus dem Textfeld abrufen
-  var eingegebenerText = kommentarTextfeld.value;
+  const eingegebenerText = kommentarTextfeld.value;
   //Anzahl der Zeichen ermitteln
-  var zeichenAnzahl = eingegebenerText.length;
+  const zeichenAnzahl = eingegebenerText.length;
   // Anzahl der Zeichen anzeigen
   zaehler.innerText = zeichenAnzahl;
 });
--- iteration3/js/favoriten.js
+++ iteration3/js/favoriten.js
@@ -1,19 +1,18 @@
 function zuFavoritenHinzufuegen(aritkel) {
-    //versuchen, das Favoriten-Array aus dem LocalStorage lesen
-    const favoriten = JSON.parse(localStorage.getItem('favoriten'));
+  //versuchen, das Favoriten-Array aus dem LocalStorage lesen
+  const favoriten = JSON.parse(localStorage.getItem("favoriten"));
 
-    // Falls das Fav.Array exisitiert, dann ...
-    if (favoriten) {
-        //Artikel zu Favoriten hinzufügen
-        favoriten.push(artikel)
-        //das aktualisierte Favoriten-Array in localStorage speichern
-        localStorage.setItem('favoriten', JSON.stringify(favoriten));
+  // Falls das Fav.Array exisitiert, dann ...
+  if (favoriten) {
+    //Artikel zu Favoriten hinzufügen
+    favoriten.push(artikel);
+    //das aktualisierte Favoriten-Array in localStorage speichern
+    localStorage.setItem("favoriten", JSON.stringify(favoriten));
 
-        // Ansonsten, wenn das Favoriten-Array noch nicht angelegt wurde, dann ...
-    } else {
-
-        //Artikel in ein neues Array einfügen und das neu erzeugte Favoriten-Array bilden
-        // Im Local-storage unter dem Schlüssel 'favoriten' speichern
-        localStorage.setItem('favoriten', JSON.stringify([artikel]));
-    }
+    // Ansonsten, wenn das Favoriten-Array noch nicht angelegt wurde, dann ...
+  } else {
+    //Artikel in ein neues Array einfügen und das neu erzeugte Favoriten-Array bilden
+    // Im Local-storage unter dem Schlüssel 'favoriten' speichern
+    localStorage.setItem("favoriten", JSON.stringify([artikel]));
+  }
 }
