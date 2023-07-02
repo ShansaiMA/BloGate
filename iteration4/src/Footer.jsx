1cad91cb15b0672af2e8203242f1e801a8366edc
Armend Ukzmaili <armend.ukzmaili@stud.hs-ruhrwest.de>
2023-07-02 20:59:28 +0200

    Header & Footer in React Syntax

--- iteration4/src/Footer.jsx
+++ iteration4/src/Footer.jsx
@@ -1,4 +1,16 @@
 import React from 'react';
 import './App.css';
 
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="fußbereich">
+        © Shansai Mahendran, Armend Ukzmaili - 2023
+      </div>
+    </footer>
+  );
+};
+
+
 export default Footer;
--- iteration4/src/Header.jsx
+++ iteration4/src/Header.jsx
@@ -3,9 +3,16 @@ import './App.css';
 
 function Header() {
   return (
-    <header>
-      {/* Hier können Sie den Inhalt des Headers definieren */}
-    </header>
+    <header className="header">
+      <nav className="navigation">
+        <ul className="nav_links">
+          <li><a href="startseite.html">Startseite</a></li>
+          <li><a href="favoriten.html">Favoriten</a></li>
+          <li><a href="impressum.html">Impressum</a></li>
+        </ul>
+        <a className="csa" href="kontakt.html">Kontakt</a>
+      </nav>
+    </header> 
   );
 }
 
