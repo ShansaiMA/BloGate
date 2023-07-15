d32458c68026267d8c7b84c774df9c6b5591183b
Shansai Mahendran <shansai.mahendran@hs-ruhrwest.de>
2023-07-15 23:18:11 +0200

    Properties Artikel

---
+++ iteration4/src/properties/Artikel1Daten.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Artikel1Daten = (props) => {
+  return (
+    <div>
+      <p>{props.inhalt}</p>
+    </div>
+  )
+}
+
+export default Artikel1Daten;
---
+++ iteration4/src/properties/Artikel2Daten.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Artikel2Daten = (props) => {
+  return (
+    <div>
+      <p>{props.inhalt}</p>
+    </div>
+  )
+}
+
+export default Artikel2Daten;
